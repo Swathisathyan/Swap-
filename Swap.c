ed2842a27342db2d4c58d571e6d881030fbfff18
Swathisathyan <82638675+Swathisathyan@users.noreply.github.com>
2021-04-16 12:39:59 +0530

    CREATED SWAP.C
    Swaps two numbers without a third variable

---
+++ Swap.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("enter two space seperated integer value:");
+scanf("%d %d",&a,&b);
+printf("Before swapping a=%d,b=%d",a,b)
+a=a+b;    
+b=a-b;   
+a=a-b;
+printf("After swapping a=%d,b=%d",a,b);
